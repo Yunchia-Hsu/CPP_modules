db1b5dcc8cd75d800c62281f540b84dc4ba1e43d
Yunchia-Hsu <yh42hsu@gmail.com>
2024-08-28 16:18:55 +0300

    tested

--- test.cpp
+++ test.cpp
@@ -2,10 +2,10 @@
 
 class car
 {
-	public: 
-	// attributes
-		std::string brand;
-		int year;
+public: 
+// attributes
+	std::string brand;
+	int year;
 
 	car(std::string b, int y)
 	{
@@ -14,9 +14,36 @@ class car
 		std::cout << "car constructed: " << brand << ", year: " << year << std::endl;	
 	}
 	
-	
+	~car()
+	{
+		std::cout << "destructor: car constructed " << brand << " ,year: " << year << std::endl;
+	}
 	
 };
+
+class sample1
+{
+public:
+	char a1;
+	int i1;
+	float f1;
+	sample1(char a1, int i1, float f1);
+	
+	~sample1(void);
+
+};
+
+
+sample1::sample1(char a1, int i1, float f1)
+{
+	this->a1 = a1;
+	this->i1 = i1;
+	this->f1 = f1;
+	std::cout << "constructed called " << a1 <<"int is: " << i1 << "and fload is: " << f1 << std::endl;
+	return ;
+};
+
+
 
 int main()
 {
@@ -24,6 +51,8 @@ int main()
 	
 
 	car yours("hehe", 2024);
+
+	sample1 sample111('h', 5, 1.1);
 	
 
 	return 0;
