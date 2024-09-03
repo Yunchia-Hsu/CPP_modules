ceb55fb47163d87a1cd07114231e67c945d065fb
Yunchia-Hsu <yh42hsu@gmail.com>
2024-09-03 17:56:32 +0300

    added ex01 header

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -1,1 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yhsu <student.hive.fi>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 09:53:33 by yhsu              #+#    #+#             */
+/*   Updated: 2024/09/03 17:28:47 by yhsu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <iostream>
+#include <string>
+
+class Contact
+{
+	private:
+		int index;
+		std::string firstName;
+		std::string lastName;
+		std::string phoneNumber;
+		std::string nickName;
+		std::string darkSecret;
+	public:
+		
+};
+
+#endif
